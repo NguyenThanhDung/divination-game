a4b1d31ccf2728bfdaf7a69fa2492bc505e23280
Nguyen Thanh Dung <dung.nguyenthanh@hotmail.com>
2021-04-24 20:40:07 +0700

    Load json data

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Answer
+{
+    public string text;
+    public string image_url;
+    public bool should_show_profile_image;
+}
--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -10,5 +10,18 @@ public class DataManager : MonoBehaviour
     {
         Debug.Log("Data File:");
         Debug.Log(dataFile.text);
+
+        Data data = JsonUtility.FromJson<Data>(dataFile.text);
+        for (int i = 0; i < data.questions.Length; i++)
+        {
+            Debug.Log("Question "+ i +": "+ data.questions[i].text);
+            for (int j = 0; j < data.questions[i].answers.Length; j++)
+            {
+                Debug.Log($"  Answer {j}:");
+                Debug.Log($"    {data.questions[i].answers[j].text}");
+                Debug.Log($"    {data.questions[i].answers[j].image_url}");
+                Debug.Log($"    {data.questions[i].answers[j].should_show_profile_image}");
+            }
+        }
     }
 }
